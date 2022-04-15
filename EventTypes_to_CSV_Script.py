7e4cfb351645274347c2f61713d92db3c5dce102
luzyong <luanberry984@gmail.com>
2022-04-15 15:54:19 -0500

    conexion a api y guardando con pandas en csv

--- EventTypes_to_CSV_Script.py
+++ EventTypes_to_CSV_Script.py
@@ -1,5 +1,6 @@
 import requests
-
+import pandas
+import json
 amp_client_id = '2be0d43ade16fa93ed67'
 
 amp_api_key = 'e624dd38-a305-405e-98d8-c615b2831b13'
@@ -11,7 +12,10 @@ request = requests.get(url, auth=(amp_client_id, amp_api_key))
 datos=request.json()
 #print(datos['data'])
 j=0
-for i in datos['data']:
+'''for i in datos['data']:
   if datos['data'][j]['event_type_id'] == stream_id:
     print(datos['data'][j]['event_type'])
   j+=1
+'''
+df = pandas.DataFrame(datos['data'])
+df.to_csv("./out.csv")
