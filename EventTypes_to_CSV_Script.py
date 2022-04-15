23b4ce57aeb784cd774f5eeac5ab5157695d5304
luzyong <luanberry984@gmail.com>
2022-04-15 17:27:32 -0500

    conexión a api, filtrando por severidad y guardando los valores especificados en formato csv

--- EventTypes_to_CSV_Script.py
+++ EventTypes_to_CSV_Script.py
@@ -6,16 +6,24 @@ amp_client_id = '2be0d43ade16fa93ed67'
 amp_api_key = 'e624dd38-a305-405e-98d8-c615b2831b13'
 stream_id = 1090519054
 
-url = 'https://api.amp.cisco.com/v1/events'
+url = 'https://api.amp.cisco.com/v1/events?start_date=2015-10-01T00%3A00%3A00%2B00%3A00'
 
 request = requests.get(url, auth=(amp_client_id, amp_api_key))
 datos=request.json()
-#print(datos['data'])
+csv = {"data":[]}
+#print(datos)
+
 j=0
-'''for i in datos['data']:
-  if datos['data'][j]['event_type_id'] == stream_id:
-    print(datos['data'][j]['event_type'])
+for i in datos['data']:
+  if 'severity' in i:
+    if i['severity']=='Low' or i['severity']=='Critical':
+      csv['data'].append({"File Name":i['file']['file_name'],
+                      "Disposition":i['file']['disposition'],
+                      "Type Event":i['event_type'],
+                      "Severity":i['severity'],
+                      "SHA256":i['file']['identity']['sha256'],
+                      "Hostname":i['computer']['hostname']})
+                      #"Filepath":i['file']})
   j+=1
-'''
-df = pandas.DataFrame(datos['data'])
+df = pandas.DataFrame.from_dict(csv['data'])
 df.to_csv("./out.csv")
