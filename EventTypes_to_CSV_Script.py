e93b31a53e4b39b7eb6295e5e288941aeffb438a
luzyong <luanberry984@gmail.com>
2022-04-18 17:43:59 -0500

    Filtrado de eventos threat detected y cloud ioc, filtrando por tres niveles de severidad para comprobar que se están registrando bien en el csv

--- EventTypes_to_CSV_Script.py
+++ EventTypes_to_CSV_Script.py
@@ -5,18 +5,18 @@ import pandas
 amp_client_id = '2be0d43ade16fa93ed67'
 amp_api_key = 'e624dd38-a305-405e-98d8-c615b2831b13'
 
-url = 'https://api.amp.cisco.com/v1/events?start_date=2015-10-01T00%3A00%3A00%2B00%3A00'
+url = 'https://api.amp.cisco.com/v1/events?event_type[]=1107296274&event_type[]=1090519054'
 
 request = requests.get(url, auth=(amp_client_id, amp_api_key))
 datos=request.json()
 csv = {"data":[]}
-print(datos)
+
 
 for i in datos['data']:
-  if datos['data']['event_type']=='Threat Detected' or datos['data']['event_type']=='Cloud IOC':
-    if i['severity']=='High' or i['severity']=='Critical':
-      if 'detection' in i and 'file_path' in i['file']:
-        csv['data'].append({"File Name":i['file']['file_name'],
+    if i['severity']=='High' or i['severity']=='Critical' or i['severity']=='Medium':
+      if 'file' in i:
+        if 'detection' in i:
+          csv['data'].append({"File Name":i['file']['file_name'],
                       "Detection":i['detection'],
                       "Disposition":i['file']['disposition'],
                       "Type Event":i['event_type'],
@@ -25,13 +25,25 @@ for i in datos['data']:
                       "Hostname":i['computer']['hostname'],
                       "File path":i['file']['file_path']
                       })
-      else:
-        csv['data'].append({"File Name":i['file']['file_name'],
+        else:
+          csv['data'].append({"File Name":i['file']['file_name'],
+                      "Detection":"",
                       "Disposition":i['file']['disposition'],
                       "Type Event":i['event_type'],
                       "Severity":i['severity'],
                       "SHA256":i['file']['identity']['sha256'],
                       "Hostname":i['computer']['hostname'],
+                      "File path":i['file']['file_path']
+                      })
+      else:
+        csv['data'].append({"File Name":i['cloud_ioc']['description'],
+                      "Detection":"",
+                      "Disposition":i['network_info']['parent']['disposition'],
+                      "Type Event":i['event_type'],
+                      "Severity":i['severity'],
+                      "SHA256":i['network_info']['parent']['identity']['sha256'],
+                      "Hostname":i['computer']['hostname'],
+                      "File path":i['network_info']['dirty_url']
                       })
 df = pandas.DataFrame.from_dict(csv['data'])
 df.to_csv("./out.csv")
