2cce735fc7d35796bd84fe2d65ba7e607ecf5483
luzyong <luanberry984@gmail.com>
2022-04-18 18:23:01 -0500

    Script final de examen, con una clase llamada File

---
+++ Events.py
@@ -0,0 +1,20 @@
+#!python
+from File import File
+
+amp_client_id = '2be0d43ade16fa93ed67'
+amp_api_key = 'e624dd38-a305-405e-98d8-c615b2831b13'
+
+def main():
+  Registro_Eventos = File(amp_client_id,amp_api_key)
+  csv=Registro_Eventos.Eventos()
+  Registro_Eventos.CSV(csv)
+
+
+if __name__=='__main__':
+  main()
+
+
+
+
+
+
